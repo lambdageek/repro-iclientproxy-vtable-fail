5f9da0a04fb30087cb08902a27e739e878e7ce00
Aleksey Kliger <alklig@microsoft.com>
2023-11-07 16:04:11 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+public interface I1 {
+    Task M1();
+}
+
+public class ClientProxy : Retrofit.IClientProxy {
+    public Task SendCoreAsync(string method, object?[] args, CancellationToken cancellationToken = default) {
+        Console.WriteLine($"SendCoreAsync - {method}");
+        return Task.CompletedTask;
+    }
+}
+
+public class P {
+    public static async Task Main()
+    {
+        var proxy = new ClientProxy();
+        var p = Retrofit.TypedClientBuilder<I1>.Build(proxy);
+        await p.M1();
+    }
+}
